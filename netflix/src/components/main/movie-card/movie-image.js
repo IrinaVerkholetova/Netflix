c2a3954f237833723aed06f7fdea1c96970b5a4d
IrinaVerkholetova <verkholetovai12@gmail.com>
2022-01-29 19:23:46 +0300

    added class component for movie-image

--- netflix/src/components/main/movie-card/movie-image.js
+++ netflix/src/components/main/movie-card/movie-image.js
@@ -4,3 +4,10 @@ import './movie-card.css';
 export const MovieImage = ({ movie }) => (
   <img src={movie.poster_path} alt={`  ${movie.title}`} className="imgMovie" />
 );
+
+export class MovieImage2 extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return <img src={movie.poster_path} alt={movie.title} className="imgMovie" />;
+  }
+}
