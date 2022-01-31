7d36e5658b41423b98acc3a43f6aa9021b8013ec
IrinaVerkholetova <verkholetovai12@gmail.com>
2022-01-31 15:34:18 +0300

    [task-2]: HOC with context

--- netflix/src/components/App.js
+++ netflix/src/components/App.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { Header } from './header/header';
+import Header from './header/header';
 import { TabsPanel } from './main/tabs/tabs-panel';
 import { Footer } from './footer/footer';
 import { LoginPage } from './login/login';
-import { AboutMovie } from './header/about-movie/about-movie';
+import AboutMovie from './header/about-movie/about-movie';
 import { WithLoading } from './loader/loader';
 
 export const StoreContext = React.createContext();
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [repos, setRepos] = useState(null);
 
   useEffect(() => {
+    let canceled = false;
+    setIsLoading(true);
     fetch(`http://localhost:4000/movies`)
       .then((json) => json.json())
       .then((repos) => {
-        setIsLoading(false);
-        setRepos({ data: repos?.data, currentUser: 'Irina Verkholetova' });
+        if (!canceled) {
+          setIsLoading(false);
+          setRepos({ data: repos?.data, currentUser: 'Irina Verkholetova' });
+        }
       });
+    return () => (canceled = true);
   }, []);
 
   return (
