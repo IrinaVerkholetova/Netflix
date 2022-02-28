5ffb97dee1649c260004d0e9cdcc6443d4df38e2
IrinaVerkholetova <verkholetovai12@gmail.com>
2022-02-28 18:06:12 +0300

    [task-4]: unit tests: actions, selectors, reducers

---
+++ netflix/src/redux/mock/data-redux.js
@@ -0,0 +1,63 @@
+export const mockDataLoading = false;
+
+export const mockDataMoviesList = [
+  {
+    title: 'La La Land',
+    vote_average: 7.9,
+    release_date: '2016-12-29',
+    poster_path:
+      'https://2.bp.blogspot.com/-mmN8FiPcWDc/WWCUELUuZzI/AAAAAAAAAGQ/ztsHb0INa1cp6JgxvG6KBxwmjtN09oLcgCLcBGAs/s1600/POSTER.jpg',
+    overview:
+      'Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.',
+    runtime: 128,
+    genres: [],
+    id: 1234,
+  },
+];
+
+export const mockDataMovie = {
+  title: 'La La Land',
+  vote_average: 7.9,
+  release_date: '2016-12-29',
+  poster_path:
+    'https://2.bp.blogspot.com/-mmN8FiPcWDc/WWCUELUuZzI/AAAAAAAAAGQ/ztsHb0INa1cp6JgxvG6KBxwmjtN09oLcgCLcBGAs/s1600/POSTER.jpg',
+  overview:
+    'Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.',
+  runtime: 128,
+  genres: [],
+  id: 1234,
+};
+
+export const mockDataError = false;
+
+export const mockDataAddMovie = {
+  title: 'La La Land',
+  vote_average: 7.9,
+  release_date: '2016-12-29',
+  poster_path:
+    'https://2.bp.blogspot.com/-mmN8FiPcWDc/WWCUELUuZzI/AAAAAAAAAGQ/ztsHb0INa1cp6JgxvG6KBxwmjtN09oLcgCLcBGAs/s1600/POSTER.jpg',
+  overview:
+    'Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.',
+  runtime: 128,
+  genres: [],
+};
+
+export const mockDataUpdateMovie = {
+  title: 'La La Land',
+  vote_average: 7.9,
+  release_date: '2016-12-29',
+  poster_path:
+    'https://2.bp.blogspot.com/-mmN8FiPcWDc/WWCUELUuZzI/AAAAAAAAAGQ/ztsHb0INa1cp6JgxvG6KBxwmjtN09oLcgCLcBGAs/s1600/POSTER.jpg',
+  overview:
+    'Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.',
+  runtime: 128,
+  genres: [],
+  id: 1234,
+};
+
+export const mockDataDeleteMovie = '1234';
+
+export const mockDataCurrentUser = {
+  login: 'Irina',
+  password: '123456',
+};
